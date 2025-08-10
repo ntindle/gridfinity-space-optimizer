9f4657135ac4e780e1fdd6646ea11b4682010b37
Nicholas Tindle <nicholas.tindle@agpt.co>
2025-08-10 02:15:11 -0500

    configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
